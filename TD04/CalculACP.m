ebdc2134ffa5f24594cbf52b1d0fdeeb7a9b63fe
Noé <emmanoe973@gmail.com>
2018-12-06 19:18:07 +0100

    add S cov(echantillon)

--- TD04/CalculACP.m
+++ TD04/CalculACP.m
@@ -12,13 +12,16 @@ function [ V ] = CalculACP(echantillon)
     %   
     
     % Settings
-    proportion_for_training = 0.1;
+    %proportion_for_training = 0.1;
     
     % Split the examples into two groups (training, testing)
-    [training_sample, testing_sample] = SplitTrainAndTest( echantillon, proportion_for_training )
+    %[training_sample, testing_sample] = SplitTrainAndTest( echantillon, proportion_for_training )
     
+    % Fetch the eigenvalues of the data covariance matrix 
+    n = size(echantillon,1);
+    S = (n-1) * cov(echantillon);
     %Fetch eigenvalues and eigenvectors
-    [vecteurs_propres,valeurs_propres]=eig(cov(echantillon));
+    [vecteurs_propres,valeurs_propres]=eig(S);
     
     %Add each eigenvalue at the top of their corresponding eigenvector matrix  
     V = diag(valeurs_propres);
